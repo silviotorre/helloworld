c920be3ed4c8b0cfb807a9c0ca80c05bc0253662
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2023-05-26 19:08:18 +0200

    Python added

---
+++ python/helloworld.py
@@ -0,0 +1,14 @@
+# this is the simplest python program to print hello world
+# to run this program, open command prompt and type python helloworld.py
+# you can also run this program in python shell
+# to open python shell, open command prompt and type python
+# to exit python shell, type exit()
+# Pro Tip: to run this program in python shell as a supercoolnerdy just type exec(open("helloworld.py").read())
+# in all the above cases, you should be in the same directory where this file is located
+# expect the unexpected
+# this program, at very end, prints Hello, World!
+print("Hello, World!")
+# Please report any bugs to the author
+# ... but dontexpectanyresponsefromauthor
+# and remember: "no matter were you go, there you are"
+# ok its finished, it was a nice journey, see you soon
