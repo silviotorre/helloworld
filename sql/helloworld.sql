2982a143b51e33caa3f3af9c6a756bfda4e3c758
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2023-06-17 09:50:15 +0200

    Sql added

---
+++ sql/helloworld.sql
@@ -0,0 +1,17 @@
+-- hello_world.sql
+
+-- This is a comment. Comments start with '--' in SQL.
+
+-- Print "Hello, World!" to the console
+SELECT 'Hello, World!';
+-- SELECT is a command that tells the database to return data to the user.
+-- 'Hello, World!' is a string literal. String literals are enclosed in single quotes.
+-- ; is a statement terminator. It tells the database that the statement is complete.
+-- SELECT statements always return a table. This SELECT statement returns a table with one row and one column.
+-- The column name is "Hello, World!".
+-- The column type is "text". Text is a string of characters.
+-- The value is "Hello, World!".
+-- The database prints the table to the console. As example in mySQL, the console is the terminal window and the table is printed in a grid.
+-- So if you want to print "Hello, World!" mysql, you can use the following command:
+-- mysql -u root -p < helloworld.sql
+-- Easy, right?
